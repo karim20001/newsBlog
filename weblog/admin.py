ffb868d4cdf153d2bab74d985c16e6d3456efd52
karim20001 <ak321402762@gmail.com>
2023-05-28 13:02:27 +0330

    register model

--- weblog/admin.py
+++ weblog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from weblog import models
 
-# Register your models here.
+admin.site.register(models.Category)
+admin.site.register(models.Comment)
+admin.site.register(models.Tag)
+admin.site.register(models.Post)
